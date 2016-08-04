ddce1cabd4eded66c1324638351f98da03acce55
chucklapress <charleslapress@gmail.com>
2016-08-04 18:44:10 -0400

    added my testpy file and pipinstalled hashids

---
+++ test.py
@@ -0,0 +1,21 @@
+from hashids import Hashids
+hashids = Hashids()
+import random
+
+def hasher():
+    hashids = Hashids(salt='www.smokingpipes.com')
+    hashid = hashids.encode(random.randint(1, 1000))
+    print(hashid)
+
+    ints = hashids.decode('mVN')
+    print(ints)
+
+
+import hashlib
+def Pyhasher():
+    mystring = input('Enter String to hash: ')
+    hash_object = hashlib.md5(mystring.encode())
+    print(hash_object.hexdigest())
+
+hasher()
+Pyhasher()
